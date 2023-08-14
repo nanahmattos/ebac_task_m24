e51e4cda254f8d9955b83c6cb30a643e0d25b199
Carolina Mattos <mattos.carolina98@gmail.com>
2023-08-14 14:10:26 -0300

    refação de código

--- main.js
+++ main.js
@@ -3,30 +3,31 @@ function AkumaNoMi (nome, tipo, poder, usuario) {
     this.tipo = tipo;
     this.poder = poder;
     this.usuario = usuario;
+
+    this.getPower = function() {
+        return `A Akuma No Mi do ${this.usuario} se chama ${this.nome}, é do tipo ${this.tipo} e tem um poder ${this.poder}.`
+    }
 }
 
 function BaraBaraNoMi (nome, tipo, poder, usuario) {
-    AkumaNoMi.call(this, nome, tipo, poder, usuario);
+    AkumaNoMi.call(this, nome, tipo, poder, usuario);    
 
-    this.power1 = function(){
-        console.log("Bara Bara Ho Kirihanashi !!");
-    }
 }
 
 function GoroGoroNoMi (nome, tipo, poder, usuario) {
     AkumaNoMi.call(this, nome, tipo, poder, usuario);
+}
 
-    this.power2 = function(){
-        console.log("30,000,000 Volt Hino !!");
-    }
+function HitoHitoNoMi (nome, tipo, poder, usuario) {
+    AkumaNoMi.call(this, nome, tipo, poder, usuario);
 }
 
 
-const AkumaNoMi_1 = new BaraBaraNoMi("BaraBara No Mi","Paramecia", "Divisão", "Buggy");
-const AkumaNoMi_2 = new GoroGoroNoMi("GoroGoro No Mi", "Logia", "Relâmpago", "Enel");
+const AkumaNoMi_1 = new BaraBaraNoMi("Bara Bara No Mi","Paramecia", "Divisão", "Buggy");
+const AkumaNoMi_2 = new GoroGoroNoMi("Goro Goro No Mi", "Logia", "Relâmpago", "Enel");
+const AkumaNoMi_3 = new HitoHitoNoMi("Hito Hito No Mi", "Zoan", "Humano", "Tony Tony Chopper");
 
-console.log(AkumaNoMi_1);
-console.log(AkumaNoMi_1.power1())
+console.log(AkumaNoMi_1.getPower())
+console.log(AkumaNoMi_2.getPower())
+console.log(AkumaNoMi_3.getPower())
 
-console.log(AkumaNoMi_2);
-console.log(AkumaNoMi_2.power2())
